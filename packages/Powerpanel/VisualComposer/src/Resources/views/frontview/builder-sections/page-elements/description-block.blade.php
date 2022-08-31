0bb7bc34ac5485b101c2170121db04755c44b50c
Himanshu Soni <64463037+hemu593@users.noreply.github.com>
2022-08-31 15:05:00 +0530

    Revert "Revert "Removed the unnecessary module from packages (31-08-2022)""
    This reverts commit 50e4f3174dce9d82378fa3f133f5fea96a916bc1.

--- packages/Powerpanel/VisualComposer/src/Resources/views/frontview/builder-sections/page-elements/description-block.blade.php
+++
@@ -1,7 +0,0 @@
-@if(isset($data['extclass']) && !empty($data['extclass']))
-    <div class="{{ $data['extclass'] }}">
-@endif
-        {!! $data['content'] !!}
-@if(isset($data['extclass']) && !empty($data['extclass']))
-    </div>    
-@endif
